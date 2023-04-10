b195ba30263727db4f5cf4f53172472360800973
fritzmg <fmg@inspiredminds.at>
2023-04-10 15:09:44 +0100

    fix translations

--- contao/languages/de/tl_news_archive.php
+++ contao/languages/de/tl_news_archive.php
@@ -16,8 +16,13 @@ $GLOBALS['TL_LANG']['tl_news_archive']['archivingTarget'] = ['Ziel', 'Das Ziel-N
 $GLOBALS['TL_LANG']['tl_news_archive']['archivingStop'] = ['Abgelaufene Einträge verschieben', 'Nachrichtenbeiträge wo eine Stop-Zeit gesetzt wurde, werden nach Ablauf automatisch in das Ziel-Archiv verschoben. Die Stop-Zeit wird im neuen Archiv entfernt, sodass der Eintrag wieder veröffentlich ist.'];
 $GLOBALS['TL_LANG']['tl_news_archive']['archivingTime'] = ['Zeit', 'Die Zeit nach der die Nachrichtenbeiträge automatisch verschoben werden. Falls keine Zeit gesetzt ist, werden die Einträge anhand der "'.$GLOBALS['TL_LANG']['tl_news_archive']['archivingStop'][0].'" Einstellung verschoben, falls aktiviert.'];
 $GLOBALS['TL_LANG']['tl_news_archive']['times'] = [
-    '1 day' => '1 Tag',
-    '1 month' => '1 Monat',
-    '1 week' => '1 Woche',
-    '1 year' => '1 Jahr',
+    'hours' => 'Stunden',
+    'days' => 'Tage',
+    'months' => 'Monate',
+    'weeks' => 'Wochen',
+    'years' => 'Jahre',
 ];
+$GLOBALS['TL_LANG']['tl_news_archive']['deletion_legend'] = 'Löschung';
+$GLOBALS['TL_LANG']['tl_news_archive']['deletion'] = ['Automatische Löschung', 'Aktiviert die automatische Löschung für dieses Nachrichtenarchiv. Die Nachrichtenbeiträge werden anhand der eingestellten Kriterien gelöscht.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['deletionStop'] = ['Abgelaufene Einträge löschen', 'Nachrichtenbeiträge wo eine Stop-Zeit gesetzt wurde, werden nach Ablauf automatisch gelöscht.'];
+$GLOBALS['TL_LANG']['tl_news_archive']['deletionTime'] = ['Zeit', 'Die Zeit nach der die Nachrichtenbeiträge automatisch gelöscht werden. Falls keine Zeit gesetzt ist, werden die Einträge anhand der "'.$GLOBALS['TL_LANG']['tl_news_archive']['deletionStop'][0].'" Einstellung gelöscht, falls aktiviert.'];
